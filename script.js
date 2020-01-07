c047eb064257982d9768f5671e361a029ef0bd94
Marcel Visser <marcel1337@hotmail.com>
2020-01-07 22:05:27 +0100

    Small styling updates

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+    Yes.
+    This file is empty.
+
+    It's quite refreshing to break free from the constraints of programming.
+    And delve into those of CSS. Why add complexity, if you don't need to? :)
+ */
