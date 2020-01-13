de8638b766edad66df44dbc41d3f73a7bd04b944
Marcel Visser <marcel1337@hotmail.com>
2020-01-13 16:39:48 +0100

    Massive updates

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+    Yes.
+    This file is pretty empty.
+
+    It's quite refreshing to break free from the constraints of programming once in a while.
+    And delve into those of CSS. Why add complexity, if you don't need to? :)
+*/
